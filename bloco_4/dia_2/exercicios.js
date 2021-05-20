3823b47a1ad3018b6926608e9ee3e202202f4c93
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-20 18:21:11 -0300

    adiciona exercicios 1, 2 e 3

---
+++ bloco_4/dia_2/exercicios.js
@@ -0,0 +1,28 @@
+
+
+//1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+}
+
+//2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+ soma += numbers[index];
+} 
+console.log(soma);
+
+//3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+let mediaAritmetica = (soma / numbers.length);
+
+console.log(mediaAritmetica);
+
+
+
