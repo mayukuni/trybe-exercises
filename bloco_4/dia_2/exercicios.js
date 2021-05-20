1217794239019e2d8dc12b069d2c5f6de54052ef
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-20 18:23:53 -0300

    adiciona exercicio 4

--- bloco_4/dia_2/exercicios.js
+++ bloco_4/dia_2/exercicios.js
@@ -24,5 +24,18 @@ let mediaAritmetica = (soma / numbers.length);
 
 console.log(mediaAritmetica);
 
+//4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+let mediaAritmetica = (soma / numbers.length);
+
+if (mediaAritmetica > 20) {
+  console.log("Valor maior que 20");
+} else {
+  console.log("Valor maior ou igual a 20");
+}
 
 
