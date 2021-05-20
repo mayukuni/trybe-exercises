de8d13f726a315516091807dbdcbe240387531f2
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-20 12:03:10 -0300

    exercicio 6

--- bloco_4/dia_1/exercises.js
+++ bloco_4/dia_1/exercises.js
@@ -22,10 +22,9 @@ console.log(num1 % num2);
 //2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 let num1 = 4; 
 let num2 = 2;
-  if (num1 > num2) {
+if (num1 > num2) {
   console.log(num1)
-}
-  else {
+}  else {
   console.log(num2)
 }
 
@@ -33,7 +32,7 @@ let num2 = 2;
 let num1 = 3; 
 let num2 = 5;
 let num3 = 7;
-  if (num1 > num2 && num1 > num3) {
+if (num1 > num2 && num1 > num3) {
   console.log(num1)
 } else if (num2 > num1 && num2 > num3) {
   console.log(num2)
@@ -55,7 +54,7 @@ console.log("zero")
 let num1 = 20;
 let num2 = -4;
 let num3 = 2;
-  if (num1 + num2 + num3 == 180) {
+if (num1 + num2 + num3 == 180) {
   console.log(true)
 }  else if (num1 < 0) {
   console.log("error")
@@ -66,3 +65,30 @@ let num3 = 2;
 }  else {
   console.log(false)
 }
+
+//6 - Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+let peça = "CAVALO";
+switch (peça.toLowerCase()) {
+  case "rei":
+    console.log("Pode mover-se em qualquer direção, porém apenas uma casa por vez");
+    break;
+  case "dama":
+    console.log("Pode mover-se em qualquer direção, quantas casas quiser, desde que estejam livres");
+    break;
+  case "torre":
+    console.log("Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.");
+    break;
+  case "bispo":
+    console.log("Move-se na diagonal, quantas casas quiser.");
+    break;
+  case "cavalo":
+    console.log("É a única peça que pode saltar sobre as outras peças do tabuleiro, sejam elas amigas ou inimigas. Ele pode andar duas casas na horizontal e uma na vertical, ou duas na vertical e uma na horizontal, uma na horizontal e duas na vertical, e assim por diante.");
+    break;
+  case "peão":
+    console.log("Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas.");
+    break;
+  default:
+    console.log('Erro');
+    break;
+}
+
