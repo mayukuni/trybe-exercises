e6b87a9ebb930562d508e273a86caa1176f86ae0
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-20 11:38:15 -0300

    exercicio 5

--- bloco_4/dia_1/exercises.js
+++ bloco_4/dia_1/exercises.js
@@ -50,3 +50,19 @@ console.log("negative")
 }  else {
 console.log("zero")
 }
+
+//5 - Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+let num1 = 20;
+let num2 = -4;
+let num3 = 2;
+  if (num1 + num2 + num3 == 180) {
+  console.log(true)
+}  else if (num1 < 0) {
+  console.log("error")
+}  else if (num2 < 0) {
+  console.log("error")
+}  else if (num3 < 0) {
+  console.log("error")
+}  else {
+  console.log(false)
+}
