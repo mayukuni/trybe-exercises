735dced9d13f88a78ae2fc419798ffaabed9681a
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-20 11:25:44 -0300

    exercicio 1

---
+++ bloco_4/dia_1/exercises.js
@@ -0,0 +1,21 @@
+//1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+let num1; 
+let num2;
+console.log(num1 + num2);
+
+let num1; 
+let num2;
+console.log(num1 - num2);
+
+let num1; 
+let num2;
+console.log(num1 * num2);
+
+let num1; 
+let num2;
+console.log(num1 / num2);
+
+let num1; 
+let num2;
+console.log(num1 % num2);
