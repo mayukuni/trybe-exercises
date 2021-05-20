9a439e41a183b21cd381507bb1e31f1692bc6c51
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-20 11:32:21 -0300

    exercicio 3

--- bloco_4/dia_1/exercises.js
+++ bloco_4/dia_1/exercises.js
@@ -20,8 +20,8 @@ let num2;
 console.log(num1 % num2);
 
 //2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
-let num1; 
-let num2;
+let num1 = 4; 
+let num2 = 2;
   if (num1 > num2) {
   console.log(num1)
 }
@@ -29,4 +29,15 @@ let num2;
   console.log(num2)
 }
 
+//3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+let num1 = 3; 
+let num2 = 5;
+let num3 = 7;
+  if (num1 > num2 && num1 > num3) {
+  console.log(num1)
+} else if (num2 > num1 && num2 > num3) {
+  console.log(num2)
+} else {
+  console.log(num3)
+}
 
