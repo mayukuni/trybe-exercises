e96afefbab18ec45ebb391414ca1e816b93714ee
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-20 11:31:36 -0300

    exercicio 2

--- bloco_4/dia_1/exercises.js
+++ bloco_4/dia_1/exercises.js
@@ -1,5 +1,4 @@
 //1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
-
 let num1; 
 let num2;
 console.log(num1 + num2);
@@ -19,3 +18,15 @@ console.log(num1 / num2);
 let num1; 
 let num2;
 console.log(num1 % num2);
+
+//2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+let num1; 
+let num2;
+  if (num1 > num2) {
+  console.log(num1)
+}
+  else {
+  console.log(num2)
+}
+
+
