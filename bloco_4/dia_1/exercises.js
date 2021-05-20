a281cb293ddb51cf6c23eefaa50d259d39ddbb26
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-20 12:28:20 -0300

    exercicio 9

--- bloco_4/dia_1/exercises.js
+++ bloco_4/dia_1/exercises.js
@@ -119,3 +119,13 @@ if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
 } else {
   console.log(false);
 }
+
+//9 - Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+let num1 = 1;
+let num2 = 2;
+let num3 = 2;
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
