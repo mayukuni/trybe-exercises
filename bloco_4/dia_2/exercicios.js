5e81523cce63206cecea9fc2c7d04b3c085a947d
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-21 15:27:34 -0300

    exercicio de conteudo 1

--- bloco_4/dia_2/exercicios.js
+++ bloco_4/dia_2/exercicios.js
@@ -89,6 +89,21 @@ for (let index = 1; index <=25; index +=1) {
 }
 console.log(array);
 
+//9 - Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+let array = [];
+for (let index = 1; index <=25; index +=1) {
+  array.push(index);
+
+  for (let indexDividido = 0; indexDividido < array.length; indexDividido +=1) {
+    console.log(array[indexDividido] / 2);
+  }
+} 
+
+
+
+
+
+
 
 
 
