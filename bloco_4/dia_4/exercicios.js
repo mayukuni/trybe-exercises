b292afd9dcfab91dc8869690e7e8c051311d6d16
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-24 19:25:45 -0300

    adiciona parte exercicio 5

--- bloco_4/dia_4/exercicios.js
+++ bloco_4/dia_4/exercicios.js
@@ -20,3 +20,25 @@ for (let key in info) {
 for (let key in info) {
   console.log(info[key]);
 }
+
+//[terminar depois] 5- Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for (let key in info) {
+  if (info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+    "Sim e Sim" === "Ambos recorrentes";
+  } 
+console.log(info[key] + " e " + info2[key]);
+}
