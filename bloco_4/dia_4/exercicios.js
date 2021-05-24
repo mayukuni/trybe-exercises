a97b155533984df91d88729b890a090ae4c9a962
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-24 18:39:34 -0300

    adiciona exercicio 2

--- bloco_4/dia_4/exercicios.js
+++ bloco_4/dia_4/exercicios.js
@@ -5,6 +5,9 @@ let info = {
 };
 
 //1- Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-console.log("Bem-vinda, " + info.personagem);
+console.log('Bem-vinda, ' + info.personagem);
 
-//2- 
+//2- Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+info.recorrente = 'Sim';
+console.log(info);
+
