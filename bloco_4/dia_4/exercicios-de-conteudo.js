8f8e00f30106172ea2c315765e20c7bb6909439f
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-24 15:47:03 -0300

    adiciona exercicios de conteudo parte 2

--- bloco_4/dia_4/exercicios-de-conteudo.js
+++ bloco_4/dia_4/exercicios-de-conteudo.js
@@ -24,3 +24,25 @@ console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a
 
 //5- Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
 console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.")
+
+//for in
+
+//1- Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+}
+for (let key in names) {
+  console.log("Olá " + names[key]);
+}
+
+//2- Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+}
+for (let key in car) {
+  console.log(key, car[key])
+}
