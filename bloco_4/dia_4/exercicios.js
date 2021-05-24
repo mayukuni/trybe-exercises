6595d225d5993910c7ac7dcfeae7e60307167a24
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-24 18:42:20 -0300

    adiciona exercicio 3

--- bloco_4/dia_4/exercicios.js
+++ bloco_4/dia_4/exercicios.js
@@ -11,3 +11,7 @@ console.log('Bem-vinda, ' + info.personagem);
 info.recorrente = 'Sim';
 console.log(info);
 
+//3- Faça um for/in que mostre todas as chaves do objeto.
+for (let key in info) {
+  console.log(key);
+}
