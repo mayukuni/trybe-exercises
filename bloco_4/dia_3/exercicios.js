36fc28e7e51fe4ddffaa7e3117fc59499af56c08
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-25 12:18:31 -0300

    adiciona exercicio 2

--- bloco_4/dia_3/exercicios.js
+++ bloco_4/dia_3/exercicios.js
@@ -1,4 +1,4 @@
-//1 - 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+//1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 let n = 5;
 let asterisco = "*"
 let linha = ""
@@ -9,4 +9,11 @@ for (let index = 0; index < n; index += 1) {
     console.log(linha)
   }
 
-
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+let n = 5;
+let asterisco = "*"
+let linha = ""
+for (let index = 0; index < n; index += 1) {
+  linha = linha + asterisco;  
+  console.log(linha)
+}
