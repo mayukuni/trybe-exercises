2486ceafde963e259b71584d83c6b511cb601ef3
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-25 11:50:17 -0300

    adiciona exercicio 11

--- bloco_4/dia_1/exercises.js
+++ bloco_4/dia_1/exercises.js
@@ -141,4 +141,31 @@ if (valor >=0 && custoTotal >= 0) {
   console.log("erro");
 }
 
+//11 - Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+let salarioBruto = 3000.00;
+let inss = 0;
+if (salarioBruto <= 1556.94) {
+  inss = salarioBruto * 0.08;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+  inss = salarioBruto * 0.09;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+  inss = salarioBruto * 0.11;
+} else {
+  inss = 570.88;
+}
+let salarioBase = salarioBruto - inss;
+let imposto = 0;
 
+if (salarioBase <= 1903.98) {
+  salarioLiquido = salarioBase;
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+  imposto = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+  imposto = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+  imposto = (salarioBase * 0.225) - 636.13;
+} else {
+  imposto = (salarioBase * 0.275) - 869.36;
+}
+let salarioLiquido = salarioBase - imposto;
+console.log(salarioLiquido);
