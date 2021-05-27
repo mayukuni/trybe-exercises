4ad8e1a23f1ceff70af3b02c6422b45acc28285a
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-27 19:15:14 -0300

    adiciona parte 3 exercicios de conteudo

--- bloco_5/js.js
+++ bloco_5/js.js
@@ -1,2 +1,6 @@
-document.getElementsByClassName('prgph')[0].style.color = 'blue'
-document.getElementsByTagName('h4')[0].style.color = 'red'
+// document.getElementsByClassName('prgph')[0].style.color = 'blue'
+// document.getElementsByTagName('h4')[0].style.color = 'red'
+document.querySelector('.prgph').style.color = 'blue';
+document.querySelector('h4').style.color = 'gray';
+document.querySelector('#page-title').innerText = 'teste';
+document.querySelectorAll('.prgph')[1].style.color = 'pink';
