d61f668488fde87c485e878fb95487c46f8aaf5c
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-05-27 20:03:27 -0300

    adiciona todos os exercicios de conteudo

---
+++ bloco_5/dia_1/script.js
@@ -0,0 +1,17 @@
+let header = document.getElementById('header-container');
+document.getElementsByTagName('h1')[0];
+header.style.backgroundColor = 'lightgreen';
+let footer = document.getElementsByTagName('footer')[0];
+footer.style.backgroundColor = 'darkgreen';
+let sec1 = document.getElementsByClassName('emergency-tasks')[0];
+sec1.style.backgroundColor = 'salmon';
+let headersec1 = document.querySelectorAll('h3')[0];
+headersec1.style.backgroundColor = 'blueviolet';
+document.getElementsByTagName('h3')[1].style.backgroundColor = 'blueviolet';
+let sec2 = document.getElementsByClassName('no-emergency-tasks')[0];
+sec2.style.backgroundColor = 'yellow';
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'gray';
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'gray';
+
+
+
