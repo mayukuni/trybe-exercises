95cda7300288987b2b5687c852f09d3c2a8c4506
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-07-22 00:11:31 -0300

    adiciona exercicios de conteudo

---
+++ bloco_11/dia_1/exercicios-de-conteudo/exercicios-de-conteudo/src/App.js
@@ -0,0 +1,10 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Component from './Component';
+
+function App() {
+  return <Component />;
+}
+
+export default App;
---
+++ bloco_11/dia_1/exercicios-de-conteudo/exercicios-de-conteudo/src/Component.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Mayumi
+        </h1>
+        <p>
+        testezinho
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Component;
